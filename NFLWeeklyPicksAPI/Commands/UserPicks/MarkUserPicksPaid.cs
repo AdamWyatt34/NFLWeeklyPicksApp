1ad87d3ea8f77a59ed91cdb251840f0695b04265
Adam Wyatt <adameagle20@hotmail.com>
2022-10-13 19:53:44 -0400

    Add update and get for unpaid user picks

---
+++ NFLWeeklyPicksAPI/Commands/UserPicks/MarkUserPicksPaid.cs
@@ -0,0 +1,36 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace NFLWeeklyPicksAPI.Commands.UserPicks;
+
+public class MarkUserPicksPaid : IRequest<Unit>
+{
+    [FromBody] public Model Body { get; set; }
+
+    public record Model(IEnumerable<UserPaidModel> Picks);
+
+    public record UserPaidModel(int UserPickId, bool IsPaid);
+
+    public class Handler : IRequestHandler<MarkUserPicksPaid, Unit>
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Handler(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Unit> Handle(MarkUserPicksPaid request, CancellationToken cancellationToken)
+        {
+            foreach (var pick in request.Body.Picks)
+            {
+                var userPick = await _db.UserPicks.FirstAsync(p => p.UserPicksId == pick.UserPickId, cancellationToken);
+                userPick.IsPaid = pick.IsPaid;
+            }
+
+            await _db.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
